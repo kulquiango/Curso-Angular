90dfe4daed9b547ae918927aab6228e9f7a20d42
Kevin Ulquiango <78171886+KevinUlquiango@users.noreply.github.com>
2022-07-11 21:11:20 -0500

    KUG-Ejemplo uso de RxJs

--- RxJS_Directivas/src/app/reactive/reactive-container/reactive-container.component.ts
+++ RxJS_Directivas/src/app/reactive/reactive-container/reactive-container.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Observable, interval, filter, take, map, Subscription } from 'rxjs';
+import { query } from '@angular/animations';
+import { Component, OnInit, OnDestroy, QueryList } from '@angular/core';
+import { Observable, interval, filter, take, map, Subscription, fromEvent } from 'rxjs';
 
 @Component({
   selector: 'app-reactive-container',
@@ -7,72 +8,88 @@ import { Observable, interval, filter, take, map, Subscription } from 'rxjs';
   styleUrls: ['./reactive-container.component.css']
 })
 export class ReactiveContainerComponent implements OnInit, OnDestroy {
-
-  miSubcription: Subscription | null = null
-  constructor() {
-    /*Ejemplo RxJS 
-    let numero = 0
-    const miObservable = new Observable<number>((observer) => {
-      
-        setInterval(() => {
-          numero++
-          observer.next(numero)
-          numero === 3 && observer.complete()
-          numero === 5 && observer.error("Se rompio ya nada ajajaja")
-        }, 1000)
-  
-      });
-  
-  
-        //forma anterior
-        miObservable.subscribe(
-          (result) => {
-            console.log(result)
-          },
-          (error) => {
-            console.error(error)
-          },
-          //  complete
-          () => {
-            console.info("Observable completed")
-          }
-        )
-     
-  
-        // forma actual
-        miObservable.subscribe({
-          next: (result) => {
-            console.log(result)
-          },
-          error: (error) => {
-            console.error(error)
-          },
-          // complete
-          complete: () => {
-            console.warn("Observable completed")
-          }
-  
-        })
-      */
-  }
-
-
-  miIntervalo: Observable<number> = interval(500).pipe(
-    map(value => value + 1),
-    take(10),/*Veces a repetir */
-    filter((value) => value % 2 === 0))
-
-  ngOnInit(): void {
-    this.miSubcription = this.miIntervalo.subscribe(
-      {
-        next: value => console.log(value),
-        complete: () => console.warn("Observable completed estos son los primeros numeros pares")
-      }
-    )
-  }
+  x: number = 0;
+  y: number = 0;
+  suscriptor: Subscription | null = null
   ngOnDestroy(): void {
-    this.miSubcription!.unsubscribe()
-    console.error("Observable destroyed");
+    this.suscriptor?.unsubscribe()
+    this.x = 0
+    this.y = 0
   }
+  ngOnInit(): void {
+    const obs = fromEvent<MouseEvent>(document.querySelector('#area')!, 'mousemove');
+    this.suscriptor = obs.subscribe((e) => {
+      this.x = e.clientX
+      this.y = e.clientY
+
+    })
+  }
+
+  // miSubcription: Subscription | null = null
+  // constructor() {
+  //   /*Ejemplo RxJS 
+  //   let numero = 0
+  //   const miObservable = new Observable<number>((observer) => {
+
+  //       setInterval(() => {
+  //         numero++
+  //         observer.next(numero)
+  //         numero === 3 && observer.complete()
+  //         numero === 5 && observer.error("Se rompio ya nada ajajaja")
+  //       }, 1000)
+
+  //     });
+
+
+  //       //forma anterior
+  //       miObservable.subscribe(
+  //         (result) => {
+  //           console.log(result)
+  //         },
+  //         (error) => {
+  //           console.error(error)
+  //         },
+  //         //  complete
+  //         () => {
+  //           console.info("Observable completed")
+  //         }
+  //       )
+
+
+  //       // forma actual
+  //       miObservable.subscribe({
+  //         next: (result) => {
+  //           console.log(result)
+  //         },
+  //         error: (error) => {
+  //           console.error(error)
+  //         },
+  //         // complete
+  //         complete: () => {
+  //           console.warn("Observable completed")
+  //         }
+
+  //       })
+  //     */
+  // }
+
+
+  // miIntervalo: Observable<number> = interval(500).pipe(
+  //   map(value => value + 1),
+  //   take(10),/*Veces a repetir */
+  //   filter((value) => value % 2 === 0))
+
+  // ngOnInit(): void {
+  //   this.miSubcription = this.miIntervalo.subscribe(
+  //     {
+  //       next: value => console.log(value),
+  //       complete: () => console.warn("Observable completed estos son los primeros numeros pares")
+  //     }
+  //   )
+  // }
+  // ngOnDestroy(): void {
+  //   this.miSubcription!.unsubscribe()
+  //   console.error("Observable destroyed");
+  // }
 
 }
