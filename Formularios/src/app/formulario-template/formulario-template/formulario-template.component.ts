f6cfe467145ac1655755cb868c6b205951ddab1d
Kevin Ulquiango <78171886+KevinUlquiango@users.noreply.github.com>
2022-07-18 20:26:39 -0500

    KUG-Finalización formulario template

--- Formularios/src/app/formulario-template/formulario-template/formulario-template.component.ts
+++ Formularios/src/app/formulario-template/formulario-template/formulario-template.component.ts
@@ -8,14 +8,65 @@ import { NgForm } from '@angular/forms';
 })
 export class FormularioTemplateComponent implements OnInit {
 
+  isValid: boolean = true;
+
   @ViewChild('miFormulario')
   miFormulario!: NgForm
 
+  initialState = {
+    proyecto: '',
+    horas: 0,
+    tecnologias: ''
+  }
+  tecnologias: Array<string> = []
+
+  proyectos: any[] = []
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  agregar(): void { console.log("Enviado correctamente"); }
+  agregar(): void {
+
+    if (this.initialState.proyecto === '' || this.initialState.tecnologias === '') {
+      this.isValid = false
+      return
+    } else {
+      this.isValid = true
+    }
+
+
+    if (this.miFormulario.controls["tecnologia"].invalid) {
+      return;
+    }
+    this.proyectos.push({
+      proyecto: this.miFormulario.controls["proyecto"].value,
+      horas: this.miFormulario.controls["horas"].value,
+      tecnologias: this.tecnologias
+
+    })
+
+    this.miFormulario.resetForm({
+      proyecto: '',
+      horas: 0,
+      tecnologias: ''
+    })
+    this.tecnologias = []
+    console.log(this.proyectos)
+
+  }
+
+  agregarTecnologia(): void {
+    if (this.miFormulario.controls["tecnologia"].invalid) {
+      return;
+    }
+
+    this.tecnologias.push(this.miFormulario.controls["tecnologia"].value)
+    this.miFormulario.resetForm({
+      ...this.miFormulario.value,
+      tecnologia: ""
+
+    })
+  }
 }
