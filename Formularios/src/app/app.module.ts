1190af320fe73b9523f50be11c01b5a2663c980c
Kevin Ulquiango <78171886+KevinUlquiango@users.noreply.github.com>
2022-07-19 21:51:56 -0500

    KUG-Refactor carpetas

--- Formularios/src/app/app.module.ts
+++ Formularios/src/app/app.module.ts
@@ -3,8 +3,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component'
-import { FormularioReactiveModule } from './formulario-reactive/formulario-reactive.module';
-import { FormularioTemplateModule } from './formulario-template/formulario-template.module';
+import { FormularioReactiveModule } from './components/formulario-reactive/formulario-reactive.module';
+import { FormularioTemplateModule } from './components/formulario-template/formulario-template.module';
+
 
 
 @NgModule({
