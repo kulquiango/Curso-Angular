78994168f0c8fe79d70db4061f91006699c64012
Kevin Ulquiango <78171886+KevinUlquiango@users.noreply.github.com>
2022-07-19 21:51:33 -0500

    KUG-Componente Table

--- Formularios/src/app/common/custom-common.module.ts
+++ Formularios/src/app/common/custom-common.module.ts
@@ -1,9 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { AlertComponent } from './alert/alert.component';
+import { TableComponent } from './table/table.component';
 @NgModule({
-  declarations: [AlertComponent],
-  imports: [
+  declarations: [AlertComponent, TableComponent],
+  imports: [CommonModule
   ],
-  exports: [AlertComponent]
+  exports: [AlertComponent, TableComponent]
 })
 export class CustomCommonModule { }
