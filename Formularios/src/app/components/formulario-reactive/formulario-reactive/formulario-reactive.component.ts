39b1833023fb28e4d76de8934f60b8ff5a58e18b
Kevin Ulquiango <78171886+KevinUlquiango@users.noreply.github.com>
2022-07-20 20:39:20 -0500

    KUG-Fin formulario reactivo

--- Formularios/src/app/components/formulario-reactive/formulario-reactive/formulario-reactive.component.ts
+++ Formularios/src/app/components/formulario-reactive/formulario-reactive/formulario-reactive.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-formulario-reactive',
@@ -15,6 +15,10 @@ export class FormularioReactiveComponent implements OnInit {
     });
     es lo mismo que esta abajo pero se usa mejor el de abajo ↓
     */
+  constructor(private fb: FormBuilder) { }
+  ngOnInit(): void {
+  }
+
   miFormulario: FormGroup = this.fb.group({
     // proyecto: this.fb.control("Valor Inicial", [Validaciones]),
     proyecto: this.fb.control("", [
@@ -27,11 +31,43 @@ export class FormularioReactiveComponent implements OnInit {
       Validators.min(1),
       Validators.max(50)
     ]),
-    teconologia: this.fb.control(''),
+    tecnologias: this.fb.array([]),
   })
-  constructor(private fb: FormBuilder) { }
 
-  ngOnInit(): void {
+  tecnologia: FormControl = this.fb.control('', [
+    Validators.required,
+    Validators.minLength(3),
+    Validators.maxLength(20)
+  ])
+
+  proyectos: any[] = []
+
+  validar(): boolean {
+    return this.miFormulario.invalid && this.miFormulario.touched
+  }
+  get tecnologias() {
+    return this.miFormulario.get("tecnologias") as FormArray
   }
 
+
+
+  agregarTecnologia(): void {
+    if (this.tecnologia.invalid) {
+      this.miFormulario.markAllAsTouched()
+      return
+    }
+    this.tecnologias.push(this.fb.control(this.tecnologia.value))
+    this.tecnologia.reset()
+    console.log(this.tecnologias.value)
+  }
+
+  agregarProyecto(): void {
+    if (this.miFormulario.invalid) {
+      this.miFormulario.markAllAsTouched()
+      return
+    }
+    this.proyectos.push(this.miFormulario.value);
+    this.miFormulario.reset()
+    this.tecnologias.clear()
+  }
 }
