7db57ccd92ba4fdab357d85453e3754a4e95171d
Kevin Ulquiango <78171886+KevinUlquiango@users.noreply.github.com>
2022-07-26 21:16:14 -0500

    KUG-Cors

--- rest_mean/index.js
+++ rest_mean/index.js
@@ -1,6 +1,6 @@
 //import external
 const express = require('express')
-
+const cors = require('cors')
 //import KUG-proyecy
 const connectDatabase = require('./database/config')
 const authRouter = require('./routers/auth')
@@ -12,6 +12,7 @@ connectDatabase()
 
 const app = express()
 app.use(express.json())
+app.use(cors())
 app.use('/auth', authRouter)
 app.use('*', express.static(__dirname + '/public'))
 
